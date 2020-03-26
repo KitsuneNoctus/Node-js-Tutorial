50911503b61396f269327449c34591e327bae759
Henry Calderon <theceintiant@gmail.com>
2020-03-25 18:03:24 -0700

    Slowly not getting it sorta.

--- m1.js
+++ m1.js
@@ -1,8 +1,14 @@
 
-module.exports ={
+// module.exports ={
+//     f1: () => {
+//         console.log('Hello');
+//     }
+// };
+
+// Function with a F1 property now
+
+export default {
     f1: () => {
         console.log('Hello');
     }
 };
-
-// Function with a F1 property now
--- m2.js
+++ m2.js
@@ -1,7 +1,5 @@
 // require('./m1.js').f1();
-
 // require('./m1.js').f1();
-
 // require('./m1.js').f1();
 
 //Can now call the function properties now
